a4cca804f7998f4f1a2662a43782ff309a4da9ca
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-02 18:37:31 -0400

    Agregando AportesBLL

---
+++ BLL/AportesBLL.cs
@@ -0,0 +1,43 @@
+﻿using Alexander_AP1_P1.DAL;
+using Alexander_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alexander_AP1_P1.BLL
+{
+    public class AportesBLL
+    {
+        private readonly Context _context;
+        public AportesBLL(Context context)
+        {
+            _context = context;
+        }
+        public bool Existe(int aportesId)
+        {
+            return _context.Aportes.Any(i => i.AporteId == aportesId);
+        }
+        public bool Insertar(Aportes aportes)
+        {
+            _context.Aportes.Add(aportes);
+            return _context.SaveChanges() > 0;
+        }
+        public bool Modificar(Aportes aportes)
+        {
+            _context.Aportes.Entry(_context.Aportes.Find(aportes.AporteId)!).State = EntityState.Detached;
+            _context.Aportes.Entry(aportes).State = EntityState.Modified;
+            return _context.SaveChanges() > 0;
+        }
+        public bool Guardar(Aportes aportes)
+        {
+            if (Existe(aportes.AporteId))
+                return Modificar(aportes);
+            else
+                return Insertar(aportes);
+        }
+        public bool Eliminar(Aportes aportes)
+        {
+            _context.Aportes.Entry(_context.Aportes.Find(aportes.AporteId)!).State = EntityState.Detached;
+            _context.Aportes.Entry(aportes).State = EntityState.Deleted;
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
